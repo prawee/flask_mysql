54139caf9c0f37583b52e3ffee6788b4e5939428
Prawee Wongsa <prawee@hotmail.com>
2019-06-22 21:33:19 +0700

    update home page and singup

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # import library
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # create instance
 app = Flask(__name__)
@@ -9,6 +9,17 @@ app = Flask(__name__)
 def main():
   # return "Welcome!"
   return render_template('index.html')
+
+@app.route("/signup")
+def singup():
+  return render_template('signup.html')
+
+@app.route("/saveSignup", methods=['POST'])
+def saveSignup():
+  _name = request.form['name']
+  _username = request.form['username']
+  _password = request.form['password']
+  print(_name, _username, _password)
 
 # run
 if __name__ == "__main__":
