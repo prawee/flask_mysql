d4bfc784ee4fa5e477887ad8ccd787e19bbaf6fb
Prawee Wongsa <prawee@hotmail.com>
2019-06-22 20:39:45 +0700

    import render_template for using

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+# import library
+from flask import Flask, render_template
+
+# create instance
 app = Flask(__name__)
 
+# defind router
 @app.route("/")
 def main():
   return "Welcome!"
 
+# run
 if __name__ == "__main__":
   app.run()
